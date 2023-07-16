261bd82752d042dde96c70816b29ac59f7d4b4dc
Chiamaka <chiamakabrowneyes@gmail.com>
2023-07-16 08:23:14 -0400

    Initial Commit

---
+++ Anya/AnyaApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnyaApp.swift
+//  Anya
+//
+//  Created by chiamakabrowneyes on 7/16/23.
+//
+
+import SwiftUI
+
+@main
+struct AnyaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
